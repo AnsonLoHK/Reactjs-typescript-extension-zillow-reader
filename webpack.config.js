148c633ee50250810a0b445ad90031fc22433c41
LAPTOP-U9LNG27H\essences24775257 <chiaochiao1106@gmail.com>
2022-11-03 18:27:55 +0800

    備註 HtmlWebpackPlugin 的每個屬性意思

--- webpack.config.js
+++ webpack.config.js
@@ -18,35 +18,37 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      // Also generate a popup.html
+      // 在dist路徑 generate a popup.html
       filename: "popup.html",
+      // 根據這個絕對路徑 生成
       template: "./src/popup-page/popup.html",
       title: "Development",
       // To include only certain chunks you can limit the chunks being used
+      // chunks name 跟 entry popup是同樣的名稱
       chunks: ["popup"],
     }),
-    new CleanWebpackPlugin(),
-    new CopyPlugin({
-      patterns: [
-        {
-          from: "source",
-          to: "dest",
-          globOptions: {
-            dot: true,
-            gitignore: true,
-          },
-        },
-        { from: "other", to: "public" },
-        {
-          from: path.resolve(__dirname, "../public1111"), //当前工作路径是在dist文件夹内，搜易这里的from就是项目目录/public文件夹内。（dist和public是同级的）
-          to: "./", //放到output文件夹下，也就是当前工作文件夹dist内
-          globOptions: {
-            dot: true,
-            gitignore: true,
-          },
-        },
-      ],
-    }),
+    // new CleanWebpackPlugin(),
+    // new CopyPlugin({
+    //   patterns: [
+    //     {
+    //       from: "source",
+    //       to: "dest",
+    //       globOptions: {
+    //         dot: true,
+    //         gitignore: true,
+    //       },
+    //     },
+    //     { from: "other", to: "public" },
+    //     {
+    //       from: path.resolve(__dirname, "../public1111"), //当前工作路径是在dist文件夹内，搜易这里的from就是项目目录/public文件夹内。（dist和public是同级的）
+    //       to: "./", //放到output文件夹下，也就是当前工作文件夹dist内
+    //       globOptions: {
+    //         dot: true,
+    //         gitignore: true,
+    //       },
+    //     },
+    //   ],
+    // }),
   ],
 
   //   devtool: "inline-source-map",
