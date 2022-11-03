1c17d281c362cace23f0f0dcfa125fd37f4bfd7d
LAPTOP-U9LNG27H\essences24775257 <chiaochiao1106@gmail.com>
2022-11-03 18:19:10 +0800

    1.HtmlWebpackPlugin / CleanWebpackPlugin /CopyPlugin 設置webpack基本雛形，清除構建資料夾 / 生成HTML 文件 / 自动复制打包文件夹dist下面 2.HtmlWebpackPlugin 定義屬性設定

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -13,16 +16,43 @@ module.exports = {
   module: {
     rules: [{ test: /\.ts(x?)$/, exclude: /node_modules/, use: ["ts-loader"] }],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      // Also generate a popup.html
+      filename: "popup.html",
+      template: "./src/popup-page/popup.html",
+      title: "Development",
+      // To include only certain chunks you can limit the chunks being used
+      chunks: ["popup"],
+    }),
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "source",
+          to: "dest",
+          globOptions: {
+            dot: true,
+            gitignore: true,
+          },
+        },
+        { from: "other", to: "public" },
+        {
+          from: path.resolve(__dirname, "../public1111"), //当前工作路径是在dist文件夹内，搜易这里的from就是项目目录/public文件夹内。（dist和public是同级的）
+          to: "./", //放到output文件夹下，也就是当前工作文件夹dist内
+          globOptions: {
+            dot: true,
+            gitignore: true,
+          },
+        },
+      ],
+    }),
+  ],
 
   //   devtool: "inline-source-map",
   //   devServer: {
   //     static: "./dist",
   //   },
-  //   plugins: [
-  //     new HtmlWebpackPlugin({
-  //       title: "Development",
-  //     }),
-  //   ],
 
   //   optimization: {
   //     runtimeChunk: "single",
