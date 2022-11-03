66da3b465f3ae2b2300b3534d25c811a57794daf
LAPTOP-U9LNG27H\essences24775257 <chiaochiao1106@gmail.com>
2022-11-03 18:50:55 +0800

    可以正常跳出extension

--- src/popup-page/popup.tsx
+++ src/popup-page/popup.tsx
@@ -4,7 +4,7 @@ import { render } from "react-dom";
 interface IProps {}
 
 export const Popup: FC<IProps> = () => {
-  return <div>pop up page </div>;
+  return <div>可以正常運作extension囉</div>;
 };
 
 render(<Popup />, document.getElementById("popup"));
