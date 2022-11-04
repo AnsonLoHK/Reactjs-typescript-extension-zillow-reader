80dc50da1f0cb47871c8dbe59eedbaa22fcc23ef
LAPTOP-U9LNG27H\essences24775257 <chiaochiao1106@gmail.com>
2022-11-04 14:41:51 +0800

    降ts-node 版本

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Reactjs-typescript-extension-zillow-reader
+
 Learn Chrome Extension, React, Typescript, Webpack all togethers
 
-11/03 
-已經安置好相關webpack套件，元件內容正常顯示在extenstion上
+11/03
+已經安置好相關 webpack 套件，元件內容正常顯示在 extenstion 上
+
+降版本:
+ts-node : 无法将“ts-node”项识别为 cmdlet
+npm i -g ts-node@8.5.4
--- src/others/basic.js
+++ src/others/basic.js
@@ -1,29 +1,6 @@
-extend type Mutation {
-  createPartnerOrganization(
-    input: CreatePartnerOrganizationInput!
-  ): PartnerOrganization
+let userName = "jack";
+let hasLoggedIn = true;
 
-}
+hasLoggedIn += "H";
 
-Mutation: {
-  createPartnerOrganization: async (
-    _: Record<never, never>,
-    { input }: MutationCreatePartnerOrganizationArgs,
-    { user }: GQLContext,
-  ) => {
-    const existingPartnerOrganization = await PartnerOrganization.findOne({
-      primaryURL: input.primaryURL,
-    });
-    if (existingPartnerOrganization) {
-      throw new Error(`${input.primaryURL} already found in database`);
-    }
-    const partnerOrganization = new PartnerOrganization({
-      ...input,
-    });
-    if (!partnerOrganization) {
-      throw new Error(`${input.primaryURL} creation failed`);
-    }
-    await partnerOrganization.save();
-    return partnerOrganization;
-  },
- 
+console.log(hasLoggedIn);
