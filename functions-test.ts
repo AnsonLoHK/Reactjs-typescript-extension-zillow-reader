feae6bf290cb801aadaa364220885a522035c153
LAPTOP-U9LNG27H\essences24775257 <chiaochiao1106@gmail.com>
2022-11-08 15:27:45 +0800

    常見ts 範例

--- functions-test.ts
+++ functions-test.ts
@@ -1,2 +1,5 @@
-import addNumbers from "./functions";
-console.log(addNumbers(1, 2));
+import { printFormat } from "./functions";
+// console.log(addNumbers(1, 2));
+// console.log(addStrings("1", "2"));
+// console.log(addStrings("a"));
+console.log(printFormat("a", "s"));
--- functions.ts
+++ functions.ts
@@ -1,5 +1,19 @@
-function addNumbers(a: number, b: number): number {
+export default function addNumbers(a: number, b: number): number {
   return a + b;
 }
 
-export default addNumbers;
+// ********************** 複數個type種類 +　其中一個func調用另一個func
+
+export const format = (title: string, param: string | number): string =>
+  `結果: ${title} ${param}`;
+
+export const printFormat = (title: string, param: string | number): void => {
+  console.log("其他", format(title, param));
+};
+
+// ********************** 運用Promise
+
+export const fetchData = (url: string): Promise<string> =>
+  Promise.resolve(`Data from ${url}`);
+
+// ********************** 傳多個字串陣列當參數
